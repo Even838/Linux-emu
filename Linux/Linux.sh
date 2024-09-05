545d594c42ad6c619c003b3a88385102ca0f28c3
Even-dev <samuca1234.oficial@gmail.com>
2024-09-05 13:49:18 -0300

    Create Linux.sh

---
+++ Linux/Linux.sh
@@ -0,0 +1,3 @@
+echo "█  █ █▄ █ █ █ ▀▄▀"                          echo "█▄ █ █ ▀█ █▄█ █ █"
+                                                  while true; do                                        read -r input                                         if [ "$input" = "exit" ]; then                            echo "Saindo..."                                          break                                                 fi                                                    eval "$input"
+                                done
